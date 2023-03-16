f1bf2ffde8ea388529b742eebe03e22dcd69c18c
Mr-rak1b <Surjo1993@yahoo.com>
2023-03-15 20:00:38 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1 ;
+        int b = (a+a);
+
+           if {(
+            System.out.println((a+a)==1);
+            System.out.println(2);)
+            else
+        System.out.println(a++);}
+  }
+}
